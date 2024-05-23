7ebd406265743ee559af4f13d78cea865fc20db4
Aさん <1107a3@gmail.com>
2024-05-23 18:49:11 +0900

    Fixed install script

--- unix/scripts/install.sh
+++ unix/scripts/install.sh
@@ -41,12 +41,12 @@ fi
 
 if [ $CUI -eq 1 ]; then
 	if [ -d "./cui/" ]; then
-		find ./cui/ -type f -name "*.sh" -exec bash {} +
+		find ./cui/ -type f -name "*.sh" -exec bash {} \;
 	fi
 fi
 if [ $GUI -eq 1 ]; then
 	if [ -d "./gui/" ]; then
-		find ./gui/ -type f -name "*.sh" -exec bash {} +
+		find ./gui/ -type f -name "*.sh" -exec bash {} \;
 	fi
 fi
 if [ $DOTFILES -eq 1 ]; then
