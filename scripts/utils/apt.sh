705064e6c0b986403dec3281c4feed98c63cd8ca
Aさん <1107a3@gmail.com>
2024-07-21 00:53:22 +0900

    Update appearance

--- scripts/utils/apt.sh
+++ scripts/utils/apt.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+cd "$(dirname "$0")" || (echo -e "\e[31mFaild cd to sh dir\e[0m" >&2 && exit 1)
+[[ ! -f ../../lib/ui/log.sh ]] && echo -e "\e[31m../../lib/ui/log.sh not found\e[0m" >&2 && exit 1
+. ../../lib/ui/log.sh
+
 apps=()
 while [[ $# -gt 0 ]]; do
 	apps+=("$1")
@@ -13,15 +17,22 @@ fi
 
 FAIL_COUNT=0
 for app in "${apps[@]}"; do
-	echo -e "\e[1;44;30m INFO \e[1;40;34m Installing $app \e[0m"
+	log_info "Installing $app..." "APT"
+	
 	if [[ $(id -u) -eq 0 ]]; then
-		if ! apt-get -qq install "$app" -y; then
+		err=$(mktemp)
+		if ! apt-get -qq install "$app" -y > "$err" 2>&1; then
 			FAIL_COUNT=$((FAIL_COUNT + 1))
+			cat "$err" >&2
 		fi
+		rm "$err"
 	else
-		if ! sudo apt-get -qq install "$app" -y; then
+		err=$(sudo mktemp)
+		if ! sudo apt-get -qq install "$app" -y 2>&1 | sudo tee "$err" > /dev/null; then
 			FAIL_COUNT=$((FAIL_COUNT + 1))
+			sudo cat "$err" >&2
 		fi
+		sudo rm "$err"
 	fi
 done
 
