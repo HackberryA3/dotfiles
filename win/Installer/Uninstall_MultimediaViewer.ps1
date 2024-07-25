8863b7544301472c5c39bf4faed229d6b89adb09
Aさん <1107a3@gmail.com>
2024-07-25 22:51:43 +0900

    Changed win installation scripts to legacy

--- win/Installer/Uninstall_MultimediaViewer.ps1
+++
@@ -1,4 +0,0 @@
-Get-AppxPackage Clipchamp.Clipchamp | Remove-AppxPackage                       # Clipchamp
-Get-AppxPackage Microsoft.Paint | Remove-AppxPackage                           # ペイント
-Get-AppxPackage Microsoft.Windows.Photos | Remove-AppxPackage                  # フォト
-Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage                       # メディアプレーヤー
--- win/Installer/Uninstall_Office.ps1
+++
@@ -1,3 +0,0 @@
-Get-AppxPackage MicrosoftTeams | Remove-AppxPackage                            # Microsoft Teams
-Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage              # Office on the Webへようこそ
-winget uninstall Microsoft.OneDrive                                            # OneDrive
