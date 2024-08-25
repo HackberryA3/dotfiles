3844768f518f2df09d32766aecc218d0e4b32116
Aさん <1107a3@gmail.com>
2024-08-25 17:24:57 +0900

    Added init fzf

--- dotfiles/__linux__mac__.bash/fzf.sh
+++ dotfiles/__linux__mac__.bash/fzf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+eval "$(fzf --bash)"
+
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796 \
 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
